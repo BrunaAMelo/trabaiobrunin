7fbe57b2d7e6c3764e6a66389c4c2868fd09d05f
Bruna Melo <117448677+BrunaAMelo@users.noreply.github.com>
2023-08-06 21:03:10 -0300

    Update project.java

--- src/main/java/gitproject/project.java
+++ src/main/java/gitproject/project.java
@@ -8,41 +8,43 @@ public class project {
         
     public class Operacoes {
 
-    public double divisao(double a,
-                        double b) {
-        if(a != 0){
-            double quociente = a / b;
-            return quociente;
-        }
-        return 0;
+            public double divisao(double a, double b) {     
+                if(a != 0){
+                    double quociente = a / b;
+                    return quociente;
+                }
+                return 0;
+            }
+        
+            public double multiplicacao(double a, double b) {
+                return a * b;
+            }
+        
+            public double soma(double x, double y) {
+                return x + y;
+            }
+        
+            public double subtracao(double x, double y) {
+                x -= y;
+                return x;
+            }
+        
+            public int restoDivisao(int divisor, int dividendo) {
+                int resto = divisor % dividendo;
+                return resto;
+            }
+        
+            public int proximoNumero(int n){
+                return n++; 
+            }
+            
+            public int anteriorNumero(int n){
+                return n--; 
+            }
+            
+            System.out.println("Oi Bruninhoo ");
+        
     }
-
-    public double multiplicacao(double a, double b) {
-        return a * b;
-    }
-
-    public double soma(double x, double y) {
-        return x + y;
-    }
-
-    public double subtracao(double x, double y) {
-        x -= y;
-        return x;
-    }
-
-    public int restoDivisao(int divisor, int dividendo) {
-        int resto = divisor % dividendo;
-        return resto;
-    }
-
-    public int proximoNumero(int n){
-        return n++; 
-    }
-    
-    public int anteriorNumero(int n){
-        return n--; 
-    }
-    System.out.println("Deu certo!");
 }
         
 
